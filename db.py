df471b75b067a3bbb6cdba31bef243337557c2b9
sofyitahm <130208937+sofyitahm@users.noreply.github.com>
2024-01-14 15:13:29 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+  page_title = "titanic!", 
+  page_icon = "?", # emoji ikon untuk ikon tab, bisa dengan "random"
+  layout = "wide", #centered atau #wide
+  menu_items = {
+    'Get Help' : None, #atau string
+    'Report a bug' : "https://www.google.com",
+    'About' : "Ini adalah about"
+  }, 
+  initial_sidebar_state = "auto", 
+)
 
